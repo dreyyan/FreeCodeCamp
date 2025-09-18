69f3472c979f008290e793519b3925e7ed3fb1cc
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-09-18 12:58:50 +0800

    Finished Drum Machine

--- Front_End_Development_Libraries/Drum_Machine/src/App.tsx
+++ Front_End_Development_Libraries/Drum_Machine/src/App.tsx
@@ -1,20 +1,24 @@
+import { useState } from "react";
 import DrumPad from "./components/DrumPad";
 
 const Home = () => {
+	const [displayText, setDisplayText] = useState("");
 return (
 	<div id="drum-machine">
-		<h1 className="vt323-bold font-bold text-4xl text-center">DRUM MACHINE</h1>
+		<h1 className="nunito-bold font-bold text-3xl text-center text-white">DRUM MACHINE</h1>
+		<div className="bg-[var(--text)]">
+			<p id="display" className="text-center mt-4 mb-6 nunito-bold text-[var(--background)]">{displayText}</p>
+		</div>
 		<div id="controls-container">
-			<DrumPad id="heater-1" letter="Q" src={"heater-1.mp3"}/>
-			<DrumPad id="heater-2" letter="W" src={"heater-2.mp3"}/>
-			<DrumPad id="heater-3" letter="E" src={"heater-3.mp3"}/>
-			<DrumPad id="heater-4" letter="A" src={"heater-4.mp3"}/>
-			<DrumPad id="clap" letter="S" src={"clap.mp3"}/>
-			<DrumPad id="open-hh" letter="D" src={"open-hh.mp3"}/>
-			<DrumPad id="kick-n-h" letter="Z" src={"kick-n-h.mp3"}/>
-			<DrumPad id="kick" letter="X" src={"kick.mp3"}/>
-			<DrumPad id="closed-hh" letter="C" src={"closed-hh.mp3"}/>
-			<p id="display"></p>
+				<DrumPad id="heater-1" letter="Q" src="heater-1.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="heater-2" letter="W" src="heater-2.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="heater-3" letter="E" src="heater-3.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="heater-4" letter="A" src="heater-4.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="clap" letter="S" src="clap.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="open-hh" letter="D" src="open-hh.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="kick-n-h" letter="Z" src="kick-n-h.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="kick" letter="X" src="kick.mp3" setDisplayText={setDisplayText} />
+				<DrumPad id="closed-hh" letter="C" src="closed-hh.mp3" setDisplayText={setDisplayText} />
 		</div>
 	</div>
 	);
--- Front_End_Development_Libraries/Drum_Machine/src/components/DrumPad.tsx
+++ Front_End_Development_Libraries/Drum_Machine/src/components/DrumPad.tsx
@@ -2,13 +2,15 @@ interface DrumPadProps {
     id: string;
     letter: string;
     src: string;
+    setDisplayText: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const DrumPad: React.FC<DrumPadProps> = ({id, letter, src}) => {
+const DrumPad: React.FC<DrumPadProps> = ({id, letter, src, setDisplayText}) => {
 	const playSound = () => {
         const audio = document.getElementById(letter) as HTMLAudioElement | null;
         if (audio) {
             audio.currentTime = 0; // Rewind to start
+            setDisplayText(id);
             audio.play();
         }
 	};
